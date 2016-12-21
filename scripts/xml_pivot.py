ca514236a0d5b81d02db54258d9f5e8afa3349ce
matchouli <mathile.poulain@ouvaton.org>
2016-12-21 19:47:38 +0100

    script pour openbeermap vers xml pivot, marche pas pour l'instant

--- scripts/xml_pivot.py
+++ scripts/xml_pivot.py
@@ -1,16 +1,15 @@
 from lxml import etree
 
 # récupération des fichiers
-cinemas = open('../donnees_XML/cinemas.xml')
-preservatifs = open('../donnees_XML/preservatifs.xml')
-facs = open('../donnees_XML/facs.xml')
-cafes = open('../donnees_XML/liste_cafe.xml')
-marches = open('../donnees_XML/liste_marches.xml')
-quartiers = open('../donnees_XML/quartiers.xml')
+cinemas = open('../donnees_xml/cinemas.xml')
+preservatifs = open('../donnees_xml/preservatifs.xml')
+facs = open('../donnees_xml/facs.xml')
+cafes = open('../donnees_xml/liste_cafe.xml')
+marches = open('../donnees_xml/liste_marches.xml')
+quartiers = open('../donnees_xml/quartiers.xml')
 
 
 megatab = {}
 for element in megatab:
-    tree = etree.parse("../donnees_XML/cinemas.xml")
+    tree = etree.parse("../donnees_xml/cinemas.xml")
     for name in tree.xpath("//nom_etablissement"):
-        
