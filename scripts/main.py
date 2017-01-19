5293593978dc96c872bf9db17df49b4fd8758ac8
matchouli <mathilde.poulain@ouvaton.org>
2017-01-19 15:26:23 +0100

    version main.py avec fichier openbeermap ok

--- scripts/main.py
+++ scripts/main.py
@@ -1,7 +1,9 @@
 #! /usr/bin/python3
 #! coding: utf-8
 
-from lxml import etree
+import xml.etree.ElementTree as ET
+from bs4 import BeautifulSoup
+import re
 
 # sudo pip3 install dicttoxml nécessaire avant
 
@@ -12,6 +14,9 @@ import openbeermap2xml
 import crous2xml
 import loyers
 import test_xml_pivot
+import openbeermap_pivot2
+from openbeermap_pivot2 import recuperation_infos
+from openbeermap_pivot2 import impression_xml_pivot
 
 # ici j'ai modifié les imports pour qu'on puisse voir plus facilement quels scripts sont utilisés où
 # aide pour le debug et la compréhension du bouzin
@@ -22,24 +27,14 @@ if __name__ == '__main__':
 
 	print("Création des fichiers xml à partir des fichiers json")
 	liste=["annuaire_immobilier_de_l_enseignement_superieur","liste-des-cafes-a-un-euro","quartier_paris", "liste_des_marches_de_quartier_a_paris","cinemas-a-paris", "distributeurspreservatifsmasculinsparis2012"]
-	# Traceback (most recent call last):
-	#   File "main.py", line 20, in <module>
-	#     main(infile, outfile)
-	#   File "/home/mathilde/Dropbox/coursS1/geoapplide/scripts/json2xml2.py", line 40, in main
-	#     output.write(xml_file)
-	# TypeError: write() argument must be str, not bytes
-	# => Erreur dûe à un json2xml2 qui est en python2 et appelé en python3
-	# les strings ne sont pas encodées pareil selon les versions
-	# => dicttoxml renvoie des bytes au lieu d'une string
-	# ~~~~~TODO : debugger~~~~ DONE, ajout ouverture en mode binaire dans json2xml2.main()
-
 
 	# ~~~~~~~ TODO : appliquer chaque fonction orig->format_pivot à chaque fichier ~~~~~~~~ OK
+	print("Traitement du fichier : ")
 	for fichier in liste:
 		infile="../donnees_brutes/"+fichier+".json"
 		outfile="../donnees_xml/"+fichier+".xml"
-		print("Traitement du fichier : ",fichier)
-		json2xml2.main(infile, outfile)
+		print("\t"+fichier)
+		# json2xml2.main(infile, outfile)
 
 	print("Récupération des données d'OpenBeerMap")
 	infile = open('../donnees_brutes/OpenBeerMap_IDF.csv','r')
@@ -51,19 +46,28 @@ if __name__ == '__main__':
 
 	print("Création du fichier xml_pivot")
 	# première étape, formalisation des xml vers un format xml_pivot
-	
 
-	#erreur dans ce bloc, incompréhensible, le xml est bien formé d'apres XML_copy_editor et le W3C validator
-	# TODO : trouver un autre parser/trouver l'erreur
-	# print("Parser le fichier OpenBeerMap xml en entrée")
-	# parser = etree.XMLParser(recover=True)
-	# tree = etree.parse('../donnees_xml/OpenBeerMap.xml')
-	# root = tree.getroot()
-	# liste_noms = root.xpath("//name/text()")
-	# liste_osm = root.xpath("//osm_id/text()")
-	# dic_infos = recuperation_infos(liste_noms, liste_osm)
-	# impression_xml_pivot(dic_infos)
-	# renvoie une erreur dans le parse du ficheir openbeermap
+	print("parser le fichier xml en entrée")
+	soup = BeautifulSoup(open("../donnees_xml/OpenBeerMap.xml"), "xml")
+	liste_noms = []
+	for nom in soup.find_all('name'):
+		nom = str(nom)
+		nom = re.sub('<name>','',nom)
+		nom = re.sub('</name>','',nom)
+		print (nom)
+		liste_noms.append(nom)
+	liste_osm = []
+	for osm_id in soup.find_all('osm_id'):
+		osm_id = str(osm_id)
+		osm_id = re.sub('<osm_id>','',osm_id)
+		osm_id = re.sub('</osm_id>','',osm_id)
+		liste_osm.append(osm_id)
+
+
+	print('création du dictionnaire qui contient nom, latitude, longitude et adresse pour chaque bar')
+	dic_infos = recuperation_infos(liste_noms, liste_osm)
+	print("Impression du fichier de sortie")
+	impression_xml_pivot(dic_infos)
 
 	# deuxième étape, utilisation des fichiers au format pivot pour la création du  fichier pivot final
 	# TODO : faire une boucle recursive sur les fichiers du dossier "../xml_formattes_pivot/" et sortir le xml_pivot dans un autre dossier pour eviter doublons si le script est relancé.
